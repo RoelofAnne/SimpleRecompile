ec674cc6b0613457c6b5fa62138b18acce09c5d1
Roelof Anne <roelofanne@gmail.com>
2018-07-28 22:01:45 +0200

    Add basic application and reprogrammer dll
    Add basic first simpel application and reprogrammer dll

---
+++ Application/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Repro;
+//Using Tag
+
+namespace Application
+{
+	public class AppRe
+    {
+        public static void Main(string[] args)
+        {
+			var CleanProgramString = Reprogrammer.DecompileCode("Application.exe");
+			string UserName = "";
+			string ComputerName = "";
+
+            if(string.IsNullOrEmpty(UserName))
+			{
+				Console.WriteLine("Hi, what is your name?");
+				UserName = Console.ReadLine();
+				CleanProgramString = CleanProgramString.Replace("string text2 = \"\";", "string text2 = \"" + UserName + "\";");
+				Console.WriteLine(Reprogrammer.CompileExecutable("Application", CleanProgramString));
+				return;//TODO restart
+			}
+			else if (string.IsNullOrEmpty(ComputerName))
+			{
+				Console.WriteLine("Hi {0}, how would you like to call me?",UserName);
+				ComputerName = Console.ReadLine();                               
+				ComputerName = "\\\"" + ComputerName + "\\\"";
+				CleanProgramString = CleanProgramString.Replace("string empty = string.Empty;", "string empty = \"" + ComputerName + "\";");
+				CleanProgramString = CleanProgramString.Replace("\\\"\";\"", "\"");
+				CleanProgramString = CleanProgramString.Replace("\"\\\"","\"");
+				CleanProgramString = CleanProgramString.Replace("Replace(\"string empty = \"", "Replace(\"string empty = \\\"");
+				CleanProgramString = CleanProgramString.Replace("\\\"\";", "\";");
+				//Console.WriteLine(CleanProgramString);
+				Console.WriteLine(Reprogrammer.CompileExecutable("Application", CleanProgramString));
+                return;//TODO restart
+			}
+
+			Console.WriteLine("Hi {0}. What can I, {1}, do for you today?", UserName, ComputerName);  
+        }
+    }
+}
---
+++ Repro/Repro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp;
+
+namespace Repro
+{
+	public static class Reprogrammer
+    {
+        public static bool CompileExecutable(string filename, String sourceCode)//sourceName)
+        {
+            CodeDomProvider provider = null;
+            bool compileOk = false;
+
+            provider = CodeDomProvider.CreateProvider("CSharp");
+
+            if (provider != null)
+            {
+                String exeName = String.Format(@"{0}.exe", filename);
+
+                CompilerParameters cp = new CompilerParameters(new[] { "System.dll", "Repro.dll" });
+
+                // Generate an executable instead of 
+                // a class library.
+                cp.GenerateExecutable = true;
+
+                // Specify the assembly file name to generate.
+                cp.OutputAssembly = exeName;
+
+                // Save the assembly as a physical file.
+                cp.GenerateInMemory = false;
+
+                // Set whether to treat all warnings as errors.
+                cp.TreatWarningsAsErrors = false;
+
+                CompilerResults cr = provider.CompileAssemblyFromSource(cp, sourceCode);
+
+                if (cr.Errors.Count > 0)
+                {
+                    // Display compilation errors.
+                    Console.WriteLine("Errors building {0} into {1}",
+                                      filename, cr.PathToAssembly);
+                    foreach (CompilerError ce in cr.Errors)
+                    {
+                        Console.WriteLine("  {0}", ce.ToString());
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    // Display a successful compilation message.
+                    Console.WriteLine("Source {0} built into {1} successfully.",
+                                      filename, cr.PathToAssembly);
+                }
+
+                // Return the results of the compilation.
+                if (cr.Errors.Count > 0)
+                {
+                    compileOk = false;
+                }
+                else
+                {
+                    compileOk = true;
+                }
+            }
+            return compileOk;
+        }
+
+        public static string DecompileCode(string applicationName)
+        {
+			CSharpDecompiler decompiler = new CSharpDecompiler(applicationName, new DecompilerSettings() { ThrowOnAssemblyResolveErrors = false, RemoveDeadCode = true });
+            return decompiler.DecompileWholeModuleAsString();
+        }
+    }
+}
